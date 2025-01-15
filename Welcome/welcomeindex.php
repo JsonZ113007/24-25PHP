f41a28306f4cce2d5961ba3a7da0a1f8242be230
20312 <jason.ziegler@vtsd.com>
2025-01-15 07:57:45 -0500

    PHP Progress

---
+++ Welcome/welcomeindex.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+
+<form action="welcomeindex.php" method="post"
+<div class="row"
+    <div class="col"
+        Name <input name="name" type="text" /><br />
+        Email <input name="email" type="text"><br />
+        <input type="submit" />
+    </div>
+</div>
+
+
+
+
+
+<?php include 'footer.php'; ?>
